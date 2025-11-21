4d8b3613a6811be851a51a70f0f27c76ec4f13ef
enerBydev <rjmemdoza.s@gmail.com>
2025-11-21 17:28:51 -0600

    chore: remove temporary code snippets file.

--- app/layouts/auth-layout.vue
+++ app/layouts/auth-layout.vue
@@ -7,7 +7,8 @@
     <div>
         <nuxt-link to="/">Home</nuxt-link>
         <nuxt-link to="/login">login</nuxt-link>
-        <nuxt-link to="/register">Register</nuxt-link>        <slot />
+        <nuxt-link to="/register">Register</nuxt-link>        
+        <slot />
         <footer>
             Auth Layout Footer
         </footer>        
